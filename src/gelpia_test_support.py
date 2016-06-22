3ddae9fdc9dc53a525449fb2fe6143cdd58a18bf
ian Briggs <ianbriggs@comcast.net>
2016-06-22 12:54:56 -0600

    FPTaylorCompat compatability added

--- src/gelpia_test_support.py
+++ src/gelpia_test_support.py
@@ -1,10 +1,17 @@
 import re
 
 def get_result(output):
-  match = re.search(r'\[([^,]+), \{', output)
-  if match:
-    return float(match.group(1))
-  else:
+  output = "".join([line for line in output.splitlines() if not line.startswith("Parsing") and not line.startswith("Solver")])
+  
+  try:
+    lst = eval(output, {'inf':float('inf')})
+  except:
+    print(output)
     return float('nan')
+
+  if type(lst[0]) is list:
+    return lst[0][1]
+  else:
+    return lst[0]
 
   
